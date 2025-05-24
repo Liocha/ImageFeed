507027065b2eea3be0ad131989155edc2f1ba802
Aleksey <mrLiocha@yandex.ru>
2025-05-24 03:51:38 +0300

    sprint_8

---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = 200
+    }
+   
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        imageListCell.configCell(for: imageListCell)
+        return imageListCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    
+}
