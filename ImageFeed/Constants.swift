ec5430eabd9e693efaff58357bc419fa2782d387
Aleksey <mrLiocha@yandex.ru>
2026-01-27 09:56:33 +0300

    save changes

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+enum Constants {
+    static let accessKey = "TX-ZmM3u6x0EVkgdHfFSouPCoZXE8aj980W9YtymaYM"
+    static let secretKey = "LyAfzo0ogT9-DfuumV-5kUkdh0k-u5j51U8mBl8tgBY"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
