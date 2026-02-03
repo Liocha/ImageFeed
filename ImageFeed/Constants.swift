b04486abd26cb4c51ce705797d605d560bbac909
Aleksey <mrLiocha@yandex.ru>
2026-02-03 22:29:00 +0300

    fix defaultBaseURLString должна быть типа String

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -5,5 +5,5 @@ enum Constants {
     static let secretKey = "LyAfzo0ogT9-DfuumV-5kUkdh0k-u5j51U8mBl8tgBY"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let defaultBaseURLString = "https://api.unsplash.com"
 }
