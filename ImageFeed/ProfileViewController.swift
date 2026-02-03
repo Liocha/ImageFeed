06b90046bab9191a6b7b690293ab3d373d3253b5
Aleksey <mrLiocha@yandex.ru>
2026-02-03 22:15:58 +0300

    fix force unwrap, implicit unwrap in ProfileViewController

--- ImageFeed/ProfileViewController.swift
+++ ImageFeed/ProfileViewController.swift
@@ -1,17 +1,17 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    private var nameLabel: UILabel!
-    private var usernameLabel: UILabel!
-    private var helloWorldLabel: UILabel!
-    private var profileImageView: UIImageView!
-    private var actionButton: UIButton!
-    
+    private let nameLabel = UILabel()
+    private let usernameLabel = UILabel()
+    private let helloWorldLabel = UILabel()
+    private let profileImageView = UIImageView()
+    private let actionButton = UIButton()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
-    
+
     private func setupUI() {
         setupProfileImage()
         setupNameLabel()
@@ -19,81 +19,110 @@ final class ProfileViewController: UIViewController {
         setupHelloWorldLabel()
         setupActionButton()
     }
-    
+
     private func setupProfileImage() {
-        profileImageView = UIImageView(image: UIImage(named: "profileImage"))
+        profileImageView.image = UIImage(named: "profileImage")
         profileImageView.tintColor = .gray
         profileImageView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(profileImageView)
-        
+
         NSLayoutConstraint.activate([
-            profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            profileImageView.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 20
+            ),
+            profileImageView.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor,
+                constant: 20
+            ),
             profileImageView.widthAnchor.constraint(equalToConstant: 70),
-            profileImageView.heightAnchor.constraint(equalToConstant: 70)
+            profileImageView.heightAnchor.constraint(equalToConstant: 70),
         ])
     }
-    
+
     private func setupNameLabel() {
-        nameLabel = UILabel()
         nameLabel.text = "Екатерина Новикова"
         nameLabel.textColor = .ypWhite
         nameLabel.font = UIFont.boldSystemFont(ofSize: 20)
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(nameLabel)
-        
+
         NSLayoutConstraint.activate([
-            nameLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
-            nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 20)
+            nameLabel.leadingAnchor.constraint(
+                equalTo: profileImageView.leadingAnchor
+            ),
+            nameLabel.topAnchor.constraint(
+                equalTo: profileImageView.bottomAnchor,
+                constant: 20
+            ),
         ])
     }
-    
+
     private func setupUsernameLabel() {
-        usernameLabel = UILabel()
         usernameLabel.text = "@ekaterina_nov"
         usernameLabel.textColor = .ypGrey
         usernameLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
         usernameLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(usernameLabel)
-        
+
         NSLayoutConstraint.activate([
-            usernameLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
-            usernameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8)
+            usernameLabel.leadingAnchor.constraint(
+                equalTo: profileImageView.leadingAnchor
+            ),
+            usernameLabel.topAnchor.constraint(
+                equalTo: nameLabel.bottomAnchor,
+                constant: 8
+            ),
         ])
     }
-    
+
     private func setupHelloWorldLabel() {
-        helloWorldLabel = UILabel()
         helloWorldLabel.text = "Hello, world!"
         helloWorldLabel.textColor = .ypWhite
-        
+
         helloWorldLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
-        
+
         helloWorldLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(helloWorldLabel)
-        
+
         NSLayoutConstraint.activate([
-            helloWorldLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
-            helloWorldLabel.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 8)
+            helloWorldLabel.leadingAnchor.constraint(
+                equalTo: profileImageView.leadingAnchor
+            ),
+            helloWorldLabel.topAnchor.constraint(
+                equalTo: usernameLabel.bottomAnchor,
+                constant: 8
+            ),
         ])
     }
-    
+
     private func setupActionButton() {
-        actionButton = UIButton.systemButton(
-            with: UIImage(systemName: "ipad.and.arrow.forward")!,
-            target: self,
-            action: #selector(didTapButton)
+
+        if let image = UIImage(systemName: "ipad.and.arrow.forward") {
+            actionButton.setImage(image, for: .normal)
+        }
+
+        actionButton.addTarget(
+            self,
+            action: #selector(didTapButton),
+            for: .touchUpInside
         )
+
         actionButton.tintColor = .ypRed
         actionButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(actionButton)
-        
+
         NSLayoutConstraint.activate([
-            actionButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            actionButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor)
+            actionButton.trailingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                constant: -20
+            ),
+            actionButton.centerYAnchor.constraint(
+                equalTo: profileImageView.centerYAnchor
+            ),
         ])
     }
-    
+
     @objc private func didTapButton() {
         // TODO: Добавить обработку нажатия кнопки
     }
