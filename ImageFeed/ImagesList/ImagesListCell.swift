654657796159865498a9b96879a8de7bf573e425
Aleksey <mrLiocha@yandex.ru>
2026-02-17 18:59:13 +0300

    srpint 12 draft

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -1,11 +1,31 @@
 import UIKit
 
+protocol ImagesListCellDelegate: AnyObject {
+    func imageListCellDidTapLike(_ cell: ImagesListCell)
+}
+
 final class ImagesListCell: UITableViewCell {
-    
+
     static let reuseIdentifier = "ImagesListCell"
-    
+
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
-    
+
+    weak var delegate: ImagesListCellDelegate?
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        cellImage.kf.cancelDownloadTask()
+        cellImage.image = nil
+    }
+
+    func setIsLiked(_ isLiked: Bool) {
+        let imageName = isLiked ? "FavoritesActive" : "FavoritesNoActive"
+        likeButton.setImage(UIImage(named: imageName), for: .normal)
+    }
+    @IBAction func likeButtonClicked(_ sender: Any) {
+        delegate?.imageListCellDidTapLike(self)
+    }
 }
---
+++ ImageFeed/ServiceAccessLayer/ProfileLogoutService/ProfileLogoutService.swift
@@ -0,0 +1,36 @@
+import Foundation
+import WebKit
+
+final class ProfileLogoutService {
+
+    static let shared = ProfileLogoutService()
+    private init() {}
+
+    func logout() {
+        OAuth2TokenStorage.shared.token = nil
+        cleanCookies()
+        cleanServices()
+    }
+
+    private func cleanCookies() {
+        HTTPCookieStorage.shared.removeCookies(since: .distantPast)
+
+        WKWebsiteDataStore.default().fetchDataRecords(
+            ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()
+        ) { records in
+            records.forEach { record in
+                WKWebsiteDataStore.default().removeData(
+                    ofTypes: record.dataTypes,
+                    for: [record],
+                    completionHandler: {}
+                )
+            }
+        }
+    }
+
+    private func cleanServices() {
+        ProfileService.shared.reset()
+        ProfileImageService.shared.reset()
+        ImagesListService.shared.reset()
+    }
+}
